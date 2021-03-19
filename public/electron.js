3a73f03e066e470d2d7cb700e86a7a4a90829450
patrickJasonDuque <patpatduque26@gmail.com>
2021-03-19 19:33:39 +0800

    added icon

--- public/electron.js
+++ public/electron.js
@@ -3,19 +3,20 @@ const path = require('path');
 const isDev = require('electron-is-dev');
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width          : 1540,
-    height         : 908,
-    webPreferences : {
-      nodeIntegration : true
-    }
-  });
+	// Create the browser window.
+	const win = new BrowserWindow({
+		width          : 1540,
+		height         : 908,
+		webPreferences : {
+			nodeIntegration : true
+		},
+		icon           : __dirname + '/icon.png'
+	});
 
-  win.maximize();
+	win.maximize();
 
-  // and load the index.html of the app.
-  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+	// and load the index.html of the app.
+	win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 }
 
 // This method will be called when Electron has finished
@@ -27,17 +28,17 @@ app.whenReady().then(createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
